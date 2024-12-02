962e586a26d56e04f393c13dcf4410faf318edaf
Markus Korn <markus.korn@gmail.com>
2024-12-02 08:39:40 +0100

    Enhance validation and error handling in day02
    - Enhance error handling in day02.zig by replacing boolean returns with Zig's error unions.
    - Update the `is_valid` method to include an `allow_skip` parameter, allowing validation with the option to skip inconsistent entries.
    - Implement and verify a new `part2` function that utilizes the `allow_skip` feature for advanced data validation.
    - Integrate `part2` execution into the main program flow with result output capabilities.

--- src/day02.zig
+++ src/day02.zig
@@ -4,24 +4,45 @@ const utils = @import("./utils.zig");
 const Line = struct {
     items: []u32,
 
-    fn is_valid(self: *Line) bool {
+    fn is_valid(self: *Line, allow_skip: bool) !bool {
         var currentValue: ?u32 = null;
         var isIncreasing: ?bool = null;
 
-        for (self.items) |value| {
+        for (self.items, 0..) |value, idx| {
             if (currentValue == null) {
                 currentValue = value;
                 continue;
             } else {
-                if (value == currentValue) return false;
+                if (value == currentValue) {
+                    if (allow_skip) {
+                        var ln1 = try self.copy_without_item(idx);
+                        var ln2 = try self.copy_without_item(idx - 1);
+                        return try ln1.is_valid(false) or try ln2.is_valid(false);
+                    }
+                    return false;
+                }
                 const currentIsIncreasing = value > currentValue.?;
                 if (isIncreasing == null) {
                     isIncreasing = currentIsIncreasing;
                 } else {
-                    if (isIncreasing != currentIsIncreasing) return false;
+                    if (isIncreasing != currentIsIncreasing) {
+                        if (allow_skip) {
+                            var ln1 = try self.copy_without_item(idx);
+                            var ln2 = try self.copy_without_item(idx - 1);
+                            return try ln1.is_valid(false) or try ln2.is_valid(false);
+                        }
+                        return false;
+                    }
                 }
                 const diff: i64 = @as(i64, currentValue.?) - @as(i64, value);
-                if (@abs(diff) > 3) return false;
+                if (@abs(diff) > 3) {
+                    if (allow_skip) {
+                        var ln1 = try self.copy_without_item(idx);
+                        var ln2 = try self.copy_without_item(idx - 1);
+                        return try ln1.is_valid(false) or try ln2.is_valid(false);
+                    }
+                    return false;
+                }
                 currentValue = value;
             }
         }
@@ -52,7 +73,23 @@ fn part1(content: []const u8) !u32 {
             try items.append(try utils.parseInt(u32, cell, 10));
         }
         var ln = Line{ .items = try items.toOwnedSlice() };
-        if (ln.is_valid()) safeCount += 1;
+        if (try ln.is_valid(false)) safeCount += 1;
+    }
+    return safeCount;
+}
+
+fn part2(content: []const u8) !u32 {
+    var safeCount: u32 = 0;
+    var lineIter = std.mem.tokenizeSequence(u8, content, "\n");
+
+    while (lineIter.next()) |line| {
+        var items = utils.List(u32).init(utils.gpa);
+        var cellIter = std.mem.tokenizeSequence(u8, line, " ");
+        while (cellIter.next()) |cell| {
+            try items.append(try utils.parseInt(u32, cell, 10));
+        }
+        var ln = Line{ .items = try items.toOwnedSlice() };
+        if (try ln.is_valid(true)) safeCount += 1;
     }
     return safeCount;
 }
@@ -61,8 +98,8 @@ pub fn main() !void {
     const content = @embedFile("./data/day02.txt");
     const result1 = try part1(content);
     utils.print("Result day 2 - part 2: {any}\n", .{result1});
-    //const result2 = try part2(content);
-    //utils.print("Result day 1 - part 2: {any}\n", .{result2});
+    const result2 = try part2(content);
+    utils.print("Result day 1 - part 2: {any}\n", .{result2});
 }
 
 test "day02 -> part1" {
@@ -84,4 +121,17 @@ test "day02 -> copy without item" {
     const ln1 = try ln.copy_without_item(1);
 
     try std.testing.expectEqualSlices(u32, ln1.items, &[_]u32{ 1, 3, 4, 5 });
+}
+
+test "day02 -> part2" {
+    const content =
+        \\7 6 4 2 1
+        \\1 2 7 8 9
+        \\9 7 6 2 1
+        \\1 3 2 4 5
+        \\8 6 4 4 1
+        \\1 3 6 7 9
+    ;
+    const result = try part2(content);
+    try std.testing.expectEqual(@as(u32, 4), result);
 }
